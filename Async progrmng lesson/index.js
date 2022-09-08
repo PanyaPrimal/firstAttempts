4ac696094b210399e30721002f841649edaf48fb
Victor <vpanya97@gmail.com>
2022-09-08 13:21:21 +0300

    add 3 states for Task class

--- Async progrmng lesson/index.js
+++ Async progrmng lesson/index.js
@@ -1,5 +1,8 @@
 class Task {
     constructor() {
+        this.state = Task.state.PENDING;
+        this.result = null;
+        this.reason = null;
         this.onComplete = [];
         this.onFail = [];
     }
@@ -12,16 +15,34 @@ class Task {
         if (typeof failCallback === 'function') {
             this.onFail.push(failCallback);
         }
+        
+        if (this.state !== Task.state.PENDING) {
+            if (this.state === Task.state.SUCCEEDED) {
+                completeCallback(this.result);
+            } else if (this.state === Task.state.FAILED) {
+                failCallback(this.reason);
+            }
+        }
     }
 
     complete(result) {
+        this.result = result;
+        this.state = Task.state.SUCCEEDED;
         this.onComplete.forEach(callback => callback(result));
     }
 
     fail(reason) {
+        this.reason = reason;
+        this.state = Task.state.FAILED;
         this.onFail.forEach(callback => callback(reason));
     }
 }
+
+Task.state = {
+    PENDING: 'PENDING',
+    SUCCEEDED: 'SUCCEEDED',
+    FAILED: 'FAILED'
+};
 
 const http = {
     get(url) {
